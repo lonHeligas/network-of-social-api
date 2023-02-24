1ec6278bf355bee265c17911fe12f47531ffc9e1
Lon Heligas <lon.heligas@gmail.com>
2023-02-24 15:14:33 -0600

    starting over

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-const { connect, connection } = require('mongoose');
-
-connect('localhost://127.0.0.1:27017/postSocialDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = connection;
--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const reactionRoutes = require('./reaction-routes');
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
-
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
-
-module.exports = router;
