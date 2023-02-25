953829a6c0ab8ad9782d44881e2707e6f6a798db
Lon Heligas <lon.heligas@gmail.com>
2023-02-25 09:02:22 -0600

    got the db to connect somehow

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const mongodb = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
-const data = require('./models/user')
+const data = require('./models/User')
+const db = require('./config/connection')
+const routes = require ('./routes');
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 
-const connectionStringURI = `mongodb://127.0.0.1:27017/inventoryDB`;
+//const connectionStringURI = `mongodb://127.0.0.1:27017/networkSocialDB`;
 
-let db;
-
+// let db;
 app.use(express.json());
 
-mongodb.connect(  
-  connectionStringURI,  
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Api server is running on port ${PORT}.`)
+  })
+})
+
+
+
+// mongodb.connect(  
+//   connectionStringURI,  
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   (err, client) => {
     
-    db = client.db();
-    app.listen(port, () => {
-      console.log(`Listening at http://localhost:${port}`);
-    });
-  }
-);
+//     db = client.db();
+//     app.listen(port, () => {
+//       console.log(`Listening at http://localhost:${port}`);
+//     });
+//   }
+// );
 
 
