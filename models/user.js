b1899432edf58f70eba4cc9ebda259c8c8877414
Lon Heligas <lon.heligas@gmail.com>
2023-02-25 19:40:02 -0600

    added todo list for myself and a README.md file

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-      required: true,      
+      required: true, 
+      trim: true     
     },
     email: {
       type: String,
